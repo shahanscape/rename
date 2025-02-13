de9969c935cad97822cc7b48bacf8b166de3b955
UP969642 <92123301+UP969642@users.noreply.github.com>
2025-02-13 20:03:27 +0000

    Implement cli options, parse flags and validate args

--- main.go
+++ main.go
@@ -1,7 +1,56 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+type RenameOptions struct {
+    dryRun      bool
+    recursive   bool
+    ignoreCase  bool
+    showChanges bool
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	opts := parseFlags()
+
+    args := flag.Args()
+    if err := run(args, opts); err != nil {
+        fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+        os.Exit(1)
+    }
+}
+
+func parseFlags() *RenameOptions {
+    opts := &RenameOptions{}
+
+    flag.BoolVar(&opts.dryRun, "n", false, "dry run - show what would be renamed")
+    flag.BoolVar(&opts.recursive, "r", false, "recursive - include subdirectories")
+    flag.BoolVar(&opts.ignoreCase, "i", false, "ignore case in pattern matching")
+    flag.BoolVar(&opts.showChanges, "v", false, "verbose - show renamed files")
+
+    flag.Parse()
+    return opts
+}
+
+func run(args []string, opts *RenameOptions) error {
+    pattern, files, err := validateArgs(args)
+    if err != nil {
+        return err
+    }
+    fmt.Println("Pattern:", pattern)
+    fmt.Println("Files:", files)
+    return nil
+}
+
+func validateArgs(args []string) (pattern string, files []string, err error) {
+    if len(args) < 1 {
+        return "", nil, fmt.Errorf("pattern argument is required")
+    }
+    if len(args) < 2 {
+        return "", nil, fmt.Errorf("at least one file argument is required")
+    }
+    return args[0], args[1:], nil
 }
